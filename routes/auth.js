9bdc50e7cd364c17c9b961d2e14775967dfccd93
michaeleliyahu <mmyy605@gmail.com>
2022-08-21 13:45:47 +0300

    add JWT when user login

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const User = require("../models/User");
 const CryptoJS = require("crypto-js");
-
+const jwt = require("jsonwebtoken");
 
 //register
 router.post("/register",async (req,res)=>{
@@ -32,11 +32,18 @@ router.post("/login", async(req,res)=>{
             process.env.PASS_SEC
             );
         const Originalpassword = hashesPassword.toString(CryptoJS.enc.Utf8);
-        Originalpassword !== req.body.password && res.status(401).json("wrong credentials");
+        Originalpassword !== req.body.password &&
+         res.status(401).json("wrong credentials");
         
-
+        const accessToken = jwt.sign({
+            id:user._id,
+            isAdmin:user.isAdmin,
+        },
+        process.env.JWT_SEC,
+        {expiresIn:"3d"}
+        );
         const {password, ...others} = user._doc;
-        res.status(200).json(others);
+        res.status(200).json({...others, accessToken});
 
     }catch(err){
         res.status(500).json(err);

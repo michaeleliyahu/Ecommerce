acfbe8f7cd1b5716cfcb3c808d78a0bec57887a1
michaeleliyahu <mmyy605@gmail.com>
2022-08-22 22:37:41 +0300

    add Login Register Product and finish Home page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,11 @@
-import Home from "./pages/Home"
+// import Home from "./pages/Home"
+// import Product from "./pages/Product";
+import Login from "./pages/Login";
+// import Register from "./pages/Register";
+// import ProductList from "./pages/ProductList";
 
 const App = () => {
-  return <Home/>;
+  return <Login/>;
 };
 
 export default App;

2217f5d17005d5749a7d8db18b1fab7843729710
michaeleliyahu <mmyy605@gmail.com>
2022-08-23 12:10:54 +0300

    add path to pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,28 @@
 import Home from "./pages/Home"
-// import Product from "./pages/Product";
-// import Cart from "./pages/Cart";
-// import Login from "./pages/Login";
-// import Register from "./pages/Register";
-// import ProductList from "./pages/ProductList";
+import ProductList from "./pages/ProductList";
+import Product from "./pages/Product";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Cart from "./pages/Cart";
 
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
 const App = () => {
-  return <Home/>;
+  return (
+  <Router>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path="/products/:category" element={<ProductList/>}/>
+      <Route path="/products/:id" element={<Product/>}/>
+      <Route path="/cart" element={<Cart/>}/>
+      <Route path="/login" element={<Login/>}/>
+      <Route path="/register" element={<Register/>}/>
+    </Routes>
+  </Router>
+  );
 };
 
 export default App;
